9c1c86d20d4f9d91bddaeb9f86e9e656667961df
chinmay866 <37858624+chinmay866@users.noreply.github.com>
2021-10-19 18:32:09 +0530

    video loading patch

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,8 @@
 
 setInterval(function(){
 
-  $(".reVideo").load(location.href + " .reVideo");
+  $("#reVideo1").load(location.href + " #reVideo1");
+  $("#reVideo2").load(location.href + " #reVideo2");
+  $("#reVideo3").load(location.href + " #reVideo3");
   console.log("Refreshing");
 }, 3000);
