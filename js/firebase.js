b23b82087fdb7a9aedd2a7e312606de717af4a44
chinmay866 <37858624+chinmay866@users.noreply.github.com>
2022-01-11 14:48:49 +0530

    firebase

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCUU8FPUB6ecaoO3ku21Fd-_GMT_ah4dyk",
+    authDomain: "stentorx-a7110.firebaseapp.com",
+    projectId: "stentorx-a7110",
+    storageBucket: "stentorx-a7110.appspot.com",
+    messagingSenderId: "652447704171",
+    appId: "1:652447704171:web:2f93d278cb25b882443d2e",
+    measurementId: "G-0DVS0YFDHE"
+  };
+
+firebase.initializeApp(firebaseConfig);
